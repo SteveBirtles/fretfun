f5371f2662a213a0e1ef8a1ac92f6284a7cfdbe2
Steve Birtles <sbirtles@farnborough.ac.uk>
2020-04-11 22:20:52 +0100

    Add files via upload

--- fretfun.js
+++ fretfun.js
@@ -1,5 +1,7 @@
 let w = 0, h = 0;
 const baseImage = new Image();
+const cheatImage = new Image();
+let cheat = false;
 
 const notes = [ "E", "F", null, "G", null, "A", null, "B", "C",
                 "B", "C", null, "D", null, "E", "F", null, "G",
@@ -43,10 +45,17 @@ function pageLoad() {
 
     baseImage.src = "Base.png";
     baseImage.onload = () => window.requestAnimationFrame(redraw);
+    cheatImage.src = "Solution.png";
 
 }
 
 function checkIt(event) {
+
+    if (event.key === " ") {
+        cheat = !cheat;
+        return;
+    }
+
     if (event.key.toUpperCase() === notes[theOne]) {
         if (theColour === "navy") {
             scores[theOne].correct++;
@@ -68,7 +77,11 @@ function redraw() {
     const left = w/2 - baseImage.width/2;
     const top = h/2 - baseImage.height/2;
 
-    context.drawImage(baseImage, left, top);
+    if (cheat) {
+        context.drawImage(cheatImage, left, top);
+    } else {
+        context.drawImage(baseImage, left, top);
+    }
 
     context.font = "14px Arial";
     context.textAlign = "center";
@@ -85,15 +98,21 @@ function redraw() {
             if (n === theOne) {
                 context.fillStyle = theColour;
             } else {
-                context.fillStyle = "grey";
+                if (scores[n].seen - scores[n].correct > 0) {
+                    context.fillStyle = "maroon";
+                } else {
+                    context.fillStyle = "grey";
+                }
             }
 
             let x = left + 42 + fret*104;
             let y = top + 93 + string*71;
 
-            context.beginPath();
-            context.arc(x, y, 30, 0, 2*Math.PI);
-            context.fill();
+            if (!(cheat && n === theOne)) {
+                context.beginPath();
+                context.arc(x, y, 30, 0, 2*Math.PI);
+                context.fill();
+            }
 
             if (scores[n].seen > 0 && theOne !== n) {
                 context.strokeText(Math.floor(100 * scores[n].correct / scores[n].seen) + "%", x, y);
